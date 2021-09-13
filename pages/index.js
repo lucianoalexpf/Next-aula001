22293e0df00e1dec93c8d37abbd499d74459130f
Luciano Alex dos Reis <lucianoalexpf@gmail.com>
2021-09-13 14:22:17 -0300

    Pagina index criada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+//vscode://vscode.github-authentication/did-authenticate?windowid=1&code=25d984e779632efb5e66&state=0b17da75-7ab4-4c2e-9f85-bcc4ecdc7acc
+
 function Home()
 {
     return(
